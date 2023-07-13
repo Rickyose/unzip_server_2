c5121e65872f7aa5af25dda2b2b8eed7eb10156a
Rickyose <rickyhaterearged@gmail.com>
2023-07-13 13:37:31 +0700

    Update vncsetup.sh

--- vncsetup.sh
+++ vncsetup.sh
@@ -6,6 +6,8 @@ cd /home/ubuntu/
 mkdir mount
 mkdir db-chia-dropbox
 ############################### Install rclone dan import config
+cd /root/
+sudo apt-get install fuse3 -y
 cd /home/ubuntu/
 wget https://www.dropbox.com/s/rofpzzh9to2o1hs/contoh_config.yaml
 curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip && unzip rclone-current-linux-amd64.zip && cd rclone-*-linux-amd64 && sudo cp rclone /usr/bin/ && sudo chown root:root /usr/bin/rclone && sudo chmod 755 /usr/bin/rclone && sudo mkdir -p /usr/local/share/man/man1 && sudo cp rclone.1 /usr/local/share/man/man1/ && sudo mandb
