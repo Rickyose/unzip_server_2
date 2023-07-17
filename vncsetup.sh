dfa021ee5e6aa3e460daf38f5fa955ceb0d69a59
Rickyose <rickyhaterearged@gmail.com>
2023-07-17 16:30:15 +0700

    Update vncsetup.sh

--- vncsetup.sh
+++ vncsetup.sh
@@ -5,6 +5,9 @@
 cd /home/ubuntu/
 mkdir mount
 mkdir db-chia-dropbox
+############################### Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 ############################### Install rclone dan import config
 cd /root/
 sudo apt-get install fuse3 -y
